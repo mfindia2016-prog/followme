3d2034492bcea33135893a3850a0a1785536750a
mfindia2016-prog <mfindia2016@gmail.com>
2026-08-22 22:02:04 +0530

    Refactor createClient to use supabaseBrowser instance

--- lib/supabase-browser.ts
+++ lib/supabase-browser.ts
@@ -1,8 +1,12 @@
 import { createBrowserClient } from "@supabase/ssr";
 
+const supabaseBrowser = createBrowserClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!
+);
+
+export { supabaseBrowser };
+
 export function createClient() {
-  return createBrowserClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!
-  );
+  return supabaseBrowser;
 }
